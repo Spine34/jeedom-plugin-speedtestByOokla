4508828894f4a85b331409620b463fd9705e968f
Spine-Jeedom <raphael.lhomme@free.fr>
2023-01-10 17:34:40 +0100

    update 2 files

--- desktop/js/speedtestByOokla.js
+++ desktop/js/speedtestByOokla.js
@@ -68,7 +68,7 @@ function addCmdToTable(_cmd) {
 	tr += '<td>'
 	if (is_numeric(_cmd.id)) {
 		tr += '<a class="btn btn-default btn-xs cmdAction" data-action="configure"><i class="fas fa-cogs"></i></a> '
-		tr += '<a class="btn btn-default btn-xs cmdAction" data-action="test"><i class="fas fa-rss"></i> {{Tester}}</a>'
+		tr += '<a class="btn btn-default btn-xs cmdAction" data-action="test"><i class="fas fa-rss"></i> Tester</a>'
 	}
 	tr += '<i class="fas fa-minus-circle pull-right cmdAction cursor" data-action="remove" title="{{Supprimer la commande}}"></i></td>'
 	tr += '</tr>'
